872ba10cec3075e4eaba1b1c019860f0c16cbd55
arturocueva81 <ajcs81@hotmail.com>
2026-04-23 17:53:10 -0500

    Reto_2: Genera 10 tarjetas y pinta las tarjetas del rango

--- tarjetas.js
+++ tarjetas.js
@@ -1,8 +1,27 @@
-function crearTarjetas(){
-    let contenido ="";
-    let divTarjetas = document.getElementById("divTarjetas");
-    for(let i= 1; i<=5; i++){
-        contenido = contenido +"<div class ='item'>"+i+"</div>";
-        divTarjetas.innerHTML = contenido;
+function crearTarjetas() {
+  let contenido = "";
+  let divTarjetas = document.getElementById("divTarjetas");
+  for (let i = 1; i <= 10; i++) {
+    contenido = contenido + "<div class ='item'>" + i + "</div>";
+    divTarjetas.innerHTML = contenido;
+  }
+}
+
+function pintarTarjetas(numDesde, numHasta) {
+  const tarjetas = document.querySelectorAll(".item");
+  
+  tarjetas.forEach((tarjeta, index) => {
+    const numero = index + 1;    
+    if (numero >= numDesde && numero <= numHasta) {
+      tarjeta.style.backgroundColor = "#3e7a41";
+      tarjeta.style.color = "white";
     }
+  });
+}
+
+function crearPintar() {
+  const numeroDesde = parseInt(document.getElementById("txtDesde").value);
+  const numeroHasta = parseInt(document.getElementById("txtHasta").value);
+  crearTarjetas();
+  pintarTarjetas(numeroDesde, numeroHasta);
 }
