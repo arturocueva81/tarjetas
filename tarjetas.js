5b701f7f907fd566a751612d99c0c37a8521e317
arturocueva81 <ajcs81@hotmail.com>
2026-04-23 19:10:39 -0500

    Reto_2: modifica rango, se agrega casilla de salto

--- tarjetas.js
+++ tarjetas.js
@@ -1,27 +1,47 @@
 function crearTarjetas() {
-  let contenido = "";
-  let divTarjetas = document.getElementById("divTarjetas");
-  for (let i = 1; i <= 10; i++) {
-    contenido = contenido + "<div class ='item'>" + i + "</div>";
-    divTarjetas.innerHTML = contenido;
+ 
+  const NUM_DESDE = parseInt(document.getElementById("txtDesde").value);
+  const NUM_HASTA  = parseInt(document.getElementById("txtHasta").value);
+  const NUM_SALTO  = parseInt(document.getElementById("txtSalto").value);
+  const DIV_TARJETAS = document.getElementById("divTarjetas");
+  
+  DIV_TARJETAS.innerHTML="";
+
+  for (let i = 1; i <= NUM_HASTA; i++) {
+    const ELEMENTO = document.createElement("div");
+    ELEMENTO.className = "item";
+    ELEMENTO.textContent = i;
+    DIV_TARJETAS.appendChild(ELEMENTO);
   }
 }
 
-function pintarTarjetas(numDesde, numHasta) {
-  const tarjetas = document.querySelectorAll(".item");
+function pintarTarjetas(numDesde, numHasta, numSalto) {
   
-  tarjetas.forEach((tarjeta, index) => {
-    const numero = index + 1;    
-    if (numero >= numDesde && numero <= numHasta) {
-      tarjeta.style.backgroundColor = "#3e7a41";
-      tarjeta.style.color = "white";
-    }
+  const NUM_DESDE= numDesde;
+  const NUM_HASTA = numHasta;
+  const NUM_SALTO = numSalto;
+  const TARJETAS = document.querySelectorAll(".item");
+
+  TARJETAS.forEach((tarjeta) => {
+    tarjeta.style.backgroundColor = "";
+    tarjeta.style.color = "";
   });
+
+  for (let n = NUM_DESDE; n <= NUM_HASTA; n += NUM_SALTO) {
+    const INDEX = n - 1;                  
+    const TARJETA = TARJETAS[INDEX];      
+    if (TARJETA) {                        
+      TARJETA.style.backgroundColor = "#3e7a41";
+      TARJETA.style.color = "white";
+    }
+  }
+  
 }
 
 function crearPintar() {
-  const numeroDesde = parseInt(document.getElementById("txtDesde").value);
-  const numeroHasta = parseInt(document.getElementById("txtHasta").value);
+  const NUM_DESDE = parseInt(document.getElementById("txtDesde").value);
+  const NUM_HASTA = parseInt(document.getElementById("txtHasta").value);
+  const NUM_SALTO = parseInt(document.getElementById("txtSalto").value);
   crearTarjetas();
-  pintarTarjetas(numeroDesde, numeroHasta);
+  pintarTarjetas(NUM_DESDE, NUM_HASTA, NUM_SALTO);
 }
